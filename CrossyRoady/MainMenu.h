283eee8e3bdde304534ecc9720346259fd631d2a
thng292 <nguyenquangthong292@gmail.com>
2023-10-23 19:29:09 +0700

    fix some typos

--- CrossyRoady/MainMenu.h
+++ CrossyRoady/MainMenu.h
@@ -4,9 +4,9 @@
 class MainMenu : public ConsoleGame::AbstractScreen {
     std::wstring_view getName() override;
     void Init(const std::any& args) override;
-    AbstractScreen* Clone() const override;
-    AbstractNavigation::NavigationRes Update(
-        float deltaTime, const AbstractNavigation* navigation
+    ConsoleGame::AbstractScreen* Clone() const override;
+    ConsoleGame::AbstractNavigation::NavigationRes Update(
+        float deltaTime, const ConsoleGame::AbstractNavigation* navigation
     ) override;
-    void Draw(AbstractCanvas* canvas) const override;
+    void Draw(ConsoleGame::AbstractCanvas* canvas) const override;
 };
