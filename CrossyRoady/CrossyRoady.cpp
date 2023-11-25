427de634ed3ac24d0c2aac2556d5a385c8567310
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2023-11-25 15:34:55 +0700

    Added title, game shall be called Crossy Roady

--- CrossyRoady/CrossyRoady.cpp
+++ CrossyRoady/CrossyRoady.cpp
@@ -52,7 +52,7 @@ auto main() -> int
     game->AddScreen(std::make_unique<Result>());
     game->AddScreen(std::make_unique<AskSave>());
 
-    game->Run(GameMap::ScreenName());
+    game->Run(MainMenu::ScreenName());
 
     return 0;
 }
